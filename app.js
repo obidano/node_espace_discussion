af2d49526f3e471cbe1f6fa3b5a9789cf1a81c08
obidano <omotetedan2@gmail.com>
2022-11-04 20:32:58 +0100

    server express  and requests #2

--- app.js
+++ app.js
@@ -22,13 +22,24 @@ app.use(bodyParser.json())
 app.use(cors());
 
 app.get('/', (req, res, next) => {
-    console.log("req body", req.body, typeof req.body)
     try {
         res.send({status: 200, message: "Welcome to my API"})
     } catch (e) {
         const status_code = e.status || 500
-        res.status(status_code).send({status: status_code, success: false})
+        res.status(status_code).send({status: status_code, success: false, error:e})
     }
+})
+
+app.get('/taux', (req, res, next) => {
+    const queryObject = url.parse(req.url, true).query
+    console.log("queryObject", queryObject)
+    if (queryObject.type === "NAT") {
+        return res.send({data: {'IPR': 0.3, 'INSS': 0.16}})
+    }
+    if (queryObject.type === "EXPAT") {
+        return res.send({data: {'IPR': 0.35, 'INSS': 0.16}})
+    }
+    return res.status(400).send({ error: "Type d'agent non reconnu"})
 })
 
 
@@ -37,13 +48,13 @@ app.get('/agent/v1', (req, res, next) => {
 
     try {
         db.all(sql, [], (err, rows) => {
-            if (err) return res.status(400).send({status: 400, success: false, error: err})
-            if (rows.length < 1) return res.status(400).send({status: 400, success: false, error: "Liste agents vide"})
+            if (err) return res.status(400).send({ error: err})
+            if (rows.length < 1) return res.status(400).send({ error: "Liste agents vide"})
             res.send({data: rows})
         })
     } catch (e) {
         const status_code = e.status || 500
-        res.status(status_code).send({status: status_code, success: false})
+        res.status(status_code).send({error:e})
     }
 })
 
@@ -52,13 +63,13 @@ app.get('/agent/v2', (req, res, next) => {
 
     try {
         db.all(sql, [], (err, rows) => {
-            if (err) return res.status(400).send({status: 400, success: false, error: err})
-            if (rows.length < 1) return res.status(400).send({status: 400, success: false, error: "Liste agents vide"})
+            if (err) return res.status(400).send({ error: err})
+            if (rows.length < 1) return res.status(400).send({ error: "Liste agents vide"})
             res.send(rows)
         })
     } catch (e) {
         const status_code = e.status || 500
-        res.status(status_code).send({status: status_code, success: false})
+        res.status(status_code).send({error:e})
     }
 })
 
@@ -67,13 +78,13 @@ app.post('/agent', (req, res) => {
     try {
         const {nom, age, salaire_brut, is_etranger, dons, pays} = req.body;
         db.run(insert_sql, [nom, age, salaire_brut, is_etranger, dons, pays], (err) => {
-            if (err) return res.status(400).send({status: 400, success: false, error: err})
+            if (err) return res.status(400).send({ error: err})
             console.log("Enregistrement agent reussi")
         })
-        res.status(201).send({status: 201, success: true, msg: "Enregistrement reussi"})
+        res.status(201).send({ msg: "Enregistrement reussi"})
     } catch (e) {
         const status_code = e.status || 500
-        res.status(status_code).send({status: status_code, success: false})
+        res.status(status_code).send({error:err})
     }
 })
 
