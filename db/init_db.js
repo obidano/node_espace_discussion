d58571ceb627f646be84689901080e1e06a2a90b
obidano <omotetedan2@gmail.com>
2023-01-09 08:00:05 +0100

    tables update

--- db/init_db.js
+++ db/init_db.js
@@ -16,16 +16,29 @@ CREATE TABLE IF NOT EXISTS ventes (ID INTEGER PRIMARY KEY,
                                  FOREIGN KEY (user_id)
                             REFERENCES user (user_id) );    
                                 `
+
+const create_locataires_sql = `
+CREATE TABLE IF NOT EXISTS locataires (ID INTEGER PRIMARY KEY, 
+                                nom VARCHAR,                            
+                                nombre INTEGER, 
+                                telephone VARCHAR
+                                 );    
+                                `
 const insert_ventes_sql = `INSERT INTO ventes (produit,prix,quantite,
                     longitude,latitude,status,user_id)
                   VALUES (?,?,?,?,?,?, ?)`
 
 const insert_user_sql = `INSERT INTO user (username,password)
                   VALUES (?,?)`
+
+const insert_locataire_sql = `INSERT INTO locataires (nom,nombre,telephone)
+                  VALUES (?,?, ?)`
 //db.run(sql)
 module.exports = {
     create_user_sql,
     create_ventes_sql,
+    create_locataires_sql,
     insert_ventes_sql,
-    insert_user_sql
+    insert_user_sql,
+    insert_locataire_sql
 };
